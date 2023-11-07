ca21811f2716bff934c9223620edb95ed699e286
Triad01 <luckypee01@gmail.com>
2023-11-07 04:54:22 +0100

    modified html css and js codes

--- app.js
+++ app.js
@@ -1,86 +1,108 @@
 "use strict";
-// const section__orders = document.querySelector(".section__orders");
+// DOM SELECTIONS ===================================
+const section__orders = document.querySelector(".section__orders");
+const section__headings = document.querySelector(".section__headings");
 
-// const DDUMMY_ORDERS = [
-//   {
-//     id: "#adafdf",
-//     name: "Brendan Criag",
-//     date: "4th October, 2006",
-//     payment_status: "Paid",
-//     total: "$500",
-//     payment_method: "credit card",
-//     order_status: "Delivered",
-//   },
-//   {
-//     id: "#aadrtd",
-//     name: "Kathryn Mckain",
-//     date: "10th November, 2023",
-//     payment_status: "awaiting authorization",
-//     total: "$1000",
-//     payment_method: "Master card",
-//     order_status: "Processing",
-//   },
-//   {
-//     id: "#fgfasd",
-//     name: "Brendan Criag",
-//     date: "4th october, 2011",
-//     payment_status: "payment_failed",
-//     total: "$2000",
-//     payment_method: "visa card",
-//     order_status: "Shipped",
-//   },
-//   {
-//     id: "#adfdfad",
-//     name: "Brendan Criag",
-//     date: "4th october, 2012",
-//     payment_status: "payment_failed",
-//     total: "$2000",
-//     payment_method: "virtual card",
-//     order_status: "Cancelled",
-//   },
-//   {
-//     id: "#fgfagv",
-//     name: "Oleg vikorigh",
-//     date: "4th october, 2023",
-//     payment_status: "paid",
-//     total: "$200",
-//     payment_method: "visa card",
-//     order_status: "Shipped",
-//   },
-//   {
-//     id: "#adfdatr",
-//     name: "Brendan Criag",
-//     date: "4th october, 2006",
-//     payment_status: "Paid",
-//     total: "$900",
-//     payment_method: "virtual card",
-//     order_status: "Delivered",
-//   },
-// ];
+const DUMMY_ORDERS = [
+  {
+    id: "#adaoif",
+    name: "Brendan Criag",
+    date: "4th Oct, 2006",
+    payment_status: "Paid",
+    total: "$500",
+    payment_method: "credit card",
+    order_status: "Delivered",
+  },
+  {
+    id: "#aadlmd",
+    name: "Kathryn Mckain",
+    date: "10th Feb, 2023",
+    payment_status: "Awaiting Authorization",
+    total: "$100",
+    payment_method: "Master card",
+    order_status: "Processing",
+  },
+  {
+    id: "#fgfasd",
+    name: "Brendan Criag",
+    date: "4th Dec, 2011",
+    payment_status: "Failed",
+    total: "$700",
+    payment_method: "visa card",
+    order_status: "Shipped",
+  },
+  {
+    id: "#adfdld",
+    name: "Brendan Criag",
+    date: "4th Apr, 2012",
+    payment_status: "Failed",
+    total: "$200",
+    payment_method: "virtual card",
+    order_status: "Cancelled",
+  },
+  {
+    id: "#fgfagv",
+    name: "Oleg vikorigh",
+    date: "4th Nov, 2023",
+    payment_status: "Paid",
+    total: "$200",
+    payment_method: "visa card",
+    order_status: "Shipped",
+  },
+  {
+    id: "#adfdatr",
+    name: "Brendan Criag",
+    date: "4th Oct, 2006",
+    payment_status: "Paid",
+    total: "$900",
+    payment_method: "virtual card",
+    order_status: "Delivered",
+  },
+  {
+    id: "#fgfagv",
+    name: "Oleg vikorigh",
+    date: "4th Nov, 2023",
+    payment_status: "Paid",
+    total: "$200",
+    payment_method: "visa card",
+    order_status: "Shipped",
+  },
+  {
+    id: "#adfdatr",
+    name: "Brendan Criag",
+    date: "4th Oct, 2006",
+    payment_status: "Paid",
+    total: "$900",
+    payment_method: "virtual card",
+    order_status: "Delivered",
+  },
+];
 
-// Dummy_ORDERS.forEach((order) => {
-//   section__orders.insertAdjacentHTML(
-//     "afterend",
-//     `  <div class="section__order">
-//     <div>
-//       <input type="checkbox" />
-//       <p>${order.id}</p>
-//     </div>
-//     <div>${order.name}</div>
-//     <div>${order.date}</div>
-//     <div>${order.payment_status}</div>
-//     <div>${order.total}</div>
-//     <div>${order.payment_method}</div>
-//     <div>${order.order_status}</div>
-//   </div>
-//     `
-//   );
-// });
+DUMMY_ORDERS.forEach((order) => {
+  section__headings.insertAdjacentHTML(
+    "afterend",
+    `  <tr class="section__order--details">
+    <th>
+      <div class="id_box">
+        <input type="checkbox" />
+        <p class="order__id">${order.id}</p>
+      </div>
+    </th>
+    <th>${order.name}</th>
+    <th>${order.date}</th>
+    <th class="payment_status">${order.payment_status}</th>
+    <th>${order.total}</th>
+    <th>${order.payment_method}</th>
+    <th class="order_status">${order.order_status}</th>
+  </tr>
+    `
+  );
+});
 
 const payment_status = document.querySelectorAll(".payment_status");
 const order_status = document.querySelectorAll(".order_status");
 
-// =====PAYMENT STATUS STYLES ===============.
+// =====PAYMENT STATUS STYLES ========================
 payment_status.forEach((status) => {
   if (status.textContent === "Paid") {
     status.classList.add("paid");
@@ -91,8 +113,7 @@ payment_status.forEach((status) => {
   }
 });
 
-// ===ORDER STATUS STYLES =========================
-
+// ===ORDER STATUS STYLES =======================================
 order_status.forEach((status) => {
   if (status.textContent === "Delivered") {
     status.classList.add("delivered");
